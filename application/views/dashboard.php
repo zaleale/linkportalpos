be3ec6f82c162d59b83afb5e3df88dc72a3d3a8e
zaleale <muhamadrizalnaufalghazi@gmail.com>
2020-10-01 13:24:43 +0700

    first commit

---
+++ application/views/dashboard.php
@@ -0,0 +1,102 @@
+<section class="content-header">
+    <h1>
+        Dashboard
+        <small>Control Panel</small>
+    </h1>
+    <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i></a></li>
+        <li class="active">Dashboard</li>
+    </ol>
+</section>
+
+<!-- Main content -->
+<section class="content">
+<div class="row">
+        <div class="col-md-3 col-sm-6 col-xs-12">
+          <div class="info-box">
+            <span class="info-box-icon bg-aqua"><i class="fa fa-archive"></i></span>
+
+            <div class="info-box-content">
+              <span class="info-box-text">Product Knowledge</span>
+              <span class="info-box-number"></span>
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+          <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+        
+        <div class="col-md-3 col-sm-6 col-xs-12">
+          <div class="info-box">
+            <span class="info-box-icon bg-yellow"><i class="ion ion-ios-people-outline"></i></span>
+
+            <div class="info-box-content">
+              <span class="info-box-text">Kendali I Pos</span>
+              <span class="info-box-number"></span>
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+          <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+
+        <div class="col-md-3 col-sm-6 col-xs-12">
+          <div class="info-box">
+            <span class="info-box-icon bg-red"><i class="fa fa-plane"></i></span>
+
+            <div class="info-box-content">
+              <span class="info-box-text">EMS Posindo</span>
+              <span class="info-box-number"></span>
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+          <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+
+        <div class="col-md-3 col-sm-6 col-xs-12">
+          <div class="info-box">
+            <span class="info-box-icon bg-purple"><i class="fa fa-headphones"></i></span>
+
+            <div class="info-box-content">
+              <span class="info-box-text">Customer Complain Handling</span>
+              <span class="info-box-number"></span>
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+          <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+<br><br>
+        <div class="col-md-3 col-sm-6 col-xs-12">
+          <div class="info-box">
+            <span class="info-box-icon bg-green"><i class="fa fa-money"></i></span>
+
+            <div class="info-box-content">
+              <span class="info-box-text">Bea Cukai</span>
+              <span class="info-box-number"></span>
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+          <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+
+        <div class="col-md-3 col-sm-6 col-xs-12">
+          <div class="info-box">
+            <span class="info-box-icon bg-grey"><i class="fa fa-sticky-note-o"></i></span>
+
+            <div class="info-box-content">
+              <span class="info-box-text">Lacak Resi</span>
+              <span class="info-box-number"></span>
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+          <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+
+      </div>
+      <!-- /.row -->
+</section>
+
---
+++ application/views/product_knowledge/product_knowledge_data.php
@@ -0,0 +1,60 @@
+<section class="content-header">
+    <h1>
+        Product Knowledge
+        <small>Informasi Produk</small>
+    </h1>
+    <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i></a></li>
+        <li class="active">Product Knowledge</li>
+    </ol>
+</section>
+
+<!-- Main content -->
+<section class="content">
+    <div class="box">
+        <div class="box-header">
+            <h3 class="box-title">Link Product Knowledge</h3>
+            <div class="pull-right">
+                <a href="<?= site_url('product_knowledge/add') ?>" class="btn btn-primary btn-flat">
+                    <i class="fa fa-plus"></i>Create
+                </a>
+            </div>
+        </div>
+        <div class="box-body table-responsive">
+            <!-- <?php print_r($row->result()) ?> -->
+            <table class="table table-bordered table-striped " id="table1">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Nama Aplikasi</th>
+                        <th>User</th>
+                        <th>Password</th>
+                        <th>Link</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php $no = 1;
+                    foreach ($row->result() as $key => $data) { ?>
+                        <tr>
+                            <td style="width:5%;"><?= $no++ ?>.</td>
+                            <td><?= $data->nama_aplikasi ?></td>
+                            <td><?= $data->user ?></td>
+                            <td><?= $data->password ?></td>
+                            <td><?= $data->link ?></td>
+                            <td class="text=center" width="180px">
+                                <a href="<?= site_url('product_knowledge/edit/' . $data->product_knowledge_id) ?>" class="btn btn-primary btn-xs">
+                                    <i class="fa fa-pencil"></i>Update
+                                </a>
+                                <a href="<?= site_url('product_knowledge/del/' . $data->product_knowledge_id) ?>" onclick="return confirm('Yakin hapus data?')" class="btn btn-danger btn-xs">
+                                    <i class="fa fa-trash"></i>Delete
+                                </a>
+                            </td>
+                        </tr>
+                    <?php
+                    } ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</section>
